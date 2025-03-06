b6bd3b5c5d567367316c39755fe36fee9f671bf0
Upayan Mazumder <upayanm3@gmail.com>
2025-03-06 19:06:31 +0530

    feat: migrate JoinNow component to CSS modules; remove legacy CSS file

--- app/src/components/JoinNow/JoinNow.js
+++ app/src/components/JoinNow/JoinNow.js
@@ -1,11 +1,11 @@
-import "./JoinNow.css";
+import  joinNowStyles from "./JoinNow.module.css";
 import CoolImage from "../../media/join-now.webp";
 
 const JoinNow = () => {
     return (
         <div>
-        <ul class="join">
-            <div class="left">
+        <ul className={joinNowStyles.join}>
+            <div className={joinNowStyles.left}>
                 <h1>
                     Looks dope? Join now!
                 </h1>
@@ -14,7 +14,7 @@ const JoinNow = () => {
                     Individuals with Premium Services
                 </p>
             </div>
-            <div class="right">
+            <div className={joinNowStyles.right}>
                 <img src={CoolImage} alt="Join Now" height="600px">
                 </img>
             </div>
