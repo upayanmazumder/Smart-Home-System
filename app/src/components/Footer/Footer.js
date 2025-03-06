3c24c45a719c900fbfdaf8a0dc5de7babfb15df1
Upayan Mazumder <upayanm3@gmail.com>
2025-03-06 19:05:14 +0530

    feat: update Footer links to include rel="noreferrer"; refactor Reviews component to use CSS modules

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
                 <div className={FooterStyles.footerColumns}>
                     <div className={FooterStyles.footerColumn}>
                         <h3>Company</h3>
-                        <a href="https://upayan.dev" target="_blank">About Us</a>
-                        <a href="https://upayan.dev" target="_blank">Blog</a>
+                        <a href="https://upayan.dev" target="_blank" rel="noreferrer" >About Us</a>
+                        <a href="https://upayan.dev" target="_blank" rel="noreferrer">Blog</a>
                         <a href="/p/terms-of-service">Terms of Service</a>
                         <a href="/p/privacy-policy">Privacy Policy</a>
                         <a href="mailto:support.aetherlink@upayan.dev">Support</a>
@@ -24,20 +24,20 @@ const Footer = () => {
                     <div className={FooterStyles.footerColumn}>
                         <h3>Resources</h3>
                         <a href="/contact" target="_blank">Contact</a>
-                        <a href="https://github.com/upayanmazumder/Smart-Home-System" target="_blank">Documentation</a>
-                        <a href="https://discord.gg/wQTZcXpcaY" target="_blank">Community</a>
+                        <a href="https://github.com/upayanmazumder/Smart-Home-System" target="_blank" rel="noreferrer">Documentation</a>
+                        <a href="https://discord.gg/wQTZcXpcaY" target="_blank" rel="noreferrer">Community</a>
                         <a href="/contact" target="_blank">Security</a>
-                        <a href="https://github.com/upayanmazumder/Smart-Home-System" target="_blank">Repository</a>
+                        <a href="https://github.com/upayanmazumder/Smart-Home-System" target="_blank" rel="noreferrer">Repository</a>
                     </div>
                 </div>
                 <div className={FooterStyles.footerBottom}>
                     <div className={FooterStyles.socialMedia}>
-                        <a href="https://www.facebook.com/upayan.mazumder" target="_blank"><i className={FooterStyles.icon}>Facebook</i></a>
-                        <a href="https://github.com/upayanmazumder" target="_blank"><i className={FooterStyles.icon}>Github</i></a>
-                        <a href="https://www.instagram.com/upayan.mazumder/" target="_blank">
+                        <a href="https://www.facebook.com/upayan.mazumder" target="_blank" rel="noreferrer"><i className={FooterStyles.icon}>Facebook</i></a>
+                        <a href="https://github.com/upayanmazumder" target="_blank" rel="noreferrer"><i className={FooterStyles.icon}>Github</i></a>
+                        <a href="https://www.instagram.com/upayan.mazumder/" target="_blank" rel="noreferrer">
                             <i class="icon">Instagram</i>
                         </a>
-                        <a href="https://www.linkedin.com/in/upayanmazumder/" target="_blank">
+                        <a href="https://www.linkedin.com/in/upayanmazumder/" target="_blank" rel="noreferrer">
                             <i class="icon">LinkedIn</i>
                         </a>
                     </div>
--- app/src/components/Reviews/Reviews.js
+++ app/src/components/Reviews/Reviews.js
@@ -1,39 +1,34 @@
-import "./Reviews.css";
+import reviewsStyles from "./Reviews.module.css";
+
 import AntonChigurh from "../../media/reviews/anton-chigurh.webp";
 import PabloEscobar from "../../media/reviews/pablo-escobar.webp";
 import HarshadMehta from "../../media/reviews/harshad-mehta.webp";
 
 const Reviews = () => {
     return (
-        <div>
-        <a class="sectionHeading" href="/#reviews">
-            <h2>Reviews</h2>
-            <p>What they say about us..</p>
-        </a>
-        <div class="review-container">
-            <div class="review-card" style={{ alignSelf: 'flex-start' }}>
-                <div class="review-card-header">
+        <div className={reviewsStyles.reviewContainer}>
+            <div className={reviewsStyles.reviewCard} style={{ alignSelf: 'flex-start' }}>
+                <div className={reviewsStyles.reviewCardHeader}>
                     <img src={AntonChigurh} alt="Javier Bardem"></img>
                     <h3>Anton Chigurh</h3>
                 </div>
                 <p>"This smart home system has made my life so much easier. Highly recommend it!"</p>
             </div>
-            <div class="review-card target" style={{ alignSelf: 'flex-end' }}>
-                <div class="review-card-header">
+            <div className={`${reviewsStyles.reviewCard} ${reviewsStyles.target}`} style={{ alignSelf: 'flex-end' }}>
+                <div className={reviewsStyles.reviewCardHeader}>
                     <img src={PabloEscobar} alt="Pablo Escobar"></img>
                     <h3>Pablo Escobar</h3>
                 </div>
                 <p>"I love the automation features. They save me so much time every day!"</p>
             </div>
-            <div class="review-card" style={{ alignSelf: 'flex-start' }}>
-                <div class="review-card-header">
+            <div className={reviewsStyles.reviewCard} style={{ alignSelf: 'flex-start' }}>
+                <div className={reviewsStyles.reviewCardHeader}>
                     <img src={HarshadMehta} alt="Harshad Mehta"></img>
                     <h3>Harshad Mehta</h3>
                 </div>
                 <p>"A game changer for my family. The security features are top-notch."</p>
             </div>
         </div>
-    </div>  
     );
 }
 
