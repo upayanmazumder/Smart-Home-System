81fe6003b0a7bc0c36bf33d181b63a530e9689b3
Upayan Mazumder <upayanm3@gmail.com>
2025-03-11 16:59:05 +0530

    allow cors from all

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
