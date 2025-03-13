0b678e793ca49c2fa915d578e5e11291914df166
Upayan Mazumder <upayanm3@gmail.com>
2025-03-13 16:54:19 +0530

    Add CORS support to the API

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const authRoutes = require("./routes/auth");
 
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use("/auth", authRoutes);
