a886dafccd146317e5eefb4b82b18e703884e02b
Upayan Mazumder <upayanm3@gmail.com>
2025-03-19 12:38:30 +0530

    Revert

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -49,10 +49,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign({ email: user.email, name: user.name }, SECRET_KEY, { expiresIn: "1h" });
     console.log(`Login successful: email=${email}, token=${token}`);
 
-    res.json({ 
-        message: `Login successful. Welcome, ${user.name} (${user.email})!`, 
-        token 
-    });
+    res.json({ message: "Login successful", token });
 });
 
 module.exports = router;
